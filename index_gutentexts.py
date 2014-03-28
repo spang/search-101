f55a0a6002e6f09fb78b26e57a450ecd1fd61237
Christine Spang <christine@spang.cc>
2014-03-28 14:40:17 -0700

    Make document indexing idempotent.
    We no longer overwrite existing indexes, and we'll update documents that
    are already indexed instead of adding them again. Note that we never
    delete documents right now.

--- index_gutentexts.py
+++ index_gutentexts.py
@@ -1,21 +1,35 @@
 #!/usr/bin/env python
+import os
 import json
 
 from datetime import datetime
 from glob import glob
 
-from whoosh.index import create_in
-from whoosh.fields import Schema, TEXT, DATETIME
+from whoosh import index
+from whoosh.fields import Schema, ID, TEXT, DATETIME
 
-schema = Schema(title=TEXT(stored=True), author=TEXT(stored=True),
-                release_date=DATETIME, language=TEXT, content=TEXT)
-ix = create_in("indexdir", schema)
+schema = Schema(id=ID(unique=True), title=TEXT(stored=True),
+                author=TEXT(stored=True), release_date=DATETIME, language=TEXT,
+                content=TEXT)
+
+INDEX_DIR = "index"
+
+if not os.path.exists(INDEX_DIR):
+    os.mkdir(INDEX_DIR)
+
+if index.exists_in(INDEX_DIR):
+    ix = index.open_dir(INDEX_DIR)
+    print "updating existing index in '{}'".format(INDEX_DIR)
+else:
+    ix = index.create_in(INDEX_DIR, schema)
+    print "creating index in '{}'".format(INDEX_DIR)
 writer = ix.writer()
 
-# load documents: each document is a meta dict, plus add the 'content' field
-# pointing to the text. also we have to make sure to decode the text properly.
 for filename in glob('gutentexts/*.json'):
+    print "processing", filename
     data = json.load(file(filename, 'r'), encoding='utf-8')
     data['release_date'] = datetime.fromtimestamp(int(data['release_date']))
-    writer.add_document(**data)
+    writer.update_document(**data)
 writer.commit()
+
+print "done!"
